27e363b642f1891edebf9a1e8a7f276531c1e1bc
Roland Saavedra <rolandsaavedra22@gmail.com>
2024-02-26 10:01:11 -0500

    temp site changes

--- src/app/background/page.tsx
+++ src/app/background/page.tsx
@@ -1,12 +1,12 @@
 import { promises as fs } from "fs";
 import { marked } from "marked";
 export default async function Page() {
-	const file = await fs.readFile(process.cwd() + "/markdown/TEST.md", "utf8");
-	const html = await marked(file);
-	return (
-		<div className="wrapper flex-col">
-			<div className="title">Background</div>
-			<div className="prose" dangerouslySetInnerHTML={{ __html: html }}></div>
-		</div>
-	);
+  const file = await fs.readFile(process.cwd() + "/markdown/TEST.md", "utf8");
+  const html = await marked(file);
+  return (
+    <div className="wrapper flex-col">
+      <div className="title">Background</div>
+      <div className="prose">This page is coming soon.</div>
+    </div>
+  );
 }
